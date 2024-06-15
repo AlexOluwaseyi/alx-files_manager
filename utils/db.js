fc2ec8958212829dab94ed455240b6a30a435b33
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-15 19:18:12 +0100

    utils/db.js - not completed

--- utils/db.js
+++ utils/db.js
@@ -1,63 +1,36 @@
-import { promisify } from 'util';
-import { createClient } from 'redis';
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-/**
- * Represents a Redis client.
- */
-
-class RedisClient {
-  /**
-   * This creates a new RedisClient instance.
-   */
+class DBClient {
   constructor() {
-    this.client = createClient();
-    this.isClientConnected = true;
-    this.client.on('error', (err) => {
-      console.error('Redis client failed to connect:', err.message || err.toString());
-      this.isClientConnected = false;
-    });
-    this.client.on('connect', () => {
-      this.isClientConnected = true;
-    });
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(uri);
+    // console.log(client);
   }
 
-  /**
-   * This checks if this client's connection to the Redis server is active.
-   * @returns {boolean}
-   */
-  isAlive() {
-    return this.isClientConnected;
+  async isAlive() {
+    try {
+      await this.client.connect();
+      this.db = this.client.db();
+      console.log(`Successfully connected to database: ${this.db.databaseName}`);
+      return true;
+    } catch (err) {
+    //   console.error('Failed to connect to MongoDB', err);
+    //   throw err;
+      return false;
+    }
   }
 
-  /**
-   * This retrieves the value of a given key.
-   * @param {String} key The key of the item to retrieve.
-   * @returns {String | Object}
-   */
-  async get(key) {
-    return promisify(this.client.GET).bind(this.client)(key);
-  }
-
-  /**
-   * This stores a key and its value along with an expiration time.
-   * @param {String} key The key of the item to store.
-   * @param {String | Number | Boolean} value The item to store.
-   * @param {Number} duration The expiration time of the item in seconds.
-   * @returns {Promise<void>}
-   */
-  async set(key, value, duration) {
-    await promisify(this.client.SETEX)
-      .bind(this.client)(key, duration, value);
-  }
-
-  /**
-   * This removes the value of a given key.
-   * @param {String} key The key of the item to remove.
-   * @returns {Promise<void>}
-   */
-  async del(key) {
-    await promisify(this.client.DEL).bind(this.client)(key);
-  }
+//   async init() {
+//     console.log(mongoClient.isConnected());
+//     await mongoClient.connect();
+//     console.log(mongoClient.isConnected());
+//   }
 }
-export const redisClient = new RedisClient();
-export default redisClient;
+
+const newdb = new DBClient();
+newdb.connect();
