385c8faedc945bd19281116bae39befc116bdab9
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-16 19:59:59 +0100

    controllers/UsersController.js

---
+++ controllers/UsersController.js
@@ -0,0 +1,35 @@
+import dbClient from '../utils/db';
+import crypto from 'crypto';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ email });
+    if (user) {
+      return res.status(400).send({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    
+    const newUser = await dbClient.db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).send({
+      id: newUser.insertedId,
+      email,
+    });
+  }
+}
+
+export default UsersController;
