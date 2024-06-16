10facde177d8da6f276c8d9c5e56ca33eaca9a06
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-16 18:48:36 +0100

    Express

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/status', (req, res) => {
+    
+});
+
+router.get('/stats', (req, res) => {
+    
+});
+
+module.exports = router
