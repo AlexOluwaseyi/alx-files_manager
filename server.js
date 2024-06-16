6ab41d47564247b34ec0fa92cfd27e2677c035ff
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-16 11:39:25 -0700

    eslint: servers.js, controller/AppController.js, routes/index.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+  console.log(`Server listening on port ${port}`);
+});
 
 export default app;
