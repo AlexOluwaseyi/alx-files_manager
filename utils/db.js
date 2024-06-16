876b108385d64454230e638bef0d185d6dc5edb6
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-16 08:51:00 -0700

    false & true

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,20 @@
-import { MongoClient } from 'mongodb';
-
-require('dotenv').config();
+import mongodb from 'mongodb';
+// eslint-disable-next-line no-unused-vars
+import Collection from 'mongodb/lib/collection';
+// import envLoader from './env_loader';
 
 class DBClient {
   constructor() {
+    // envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const uri = `mongodb://${host}:${port}/${database}`;
+    const dbURL = `mongodb://${host}:${port}/${database}`;
 
-    this.client = new MongoClient(`mongodb://${host}:${port}/${database}`);
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
     this.client.connect()
       .then(() => {
+        console.log('DB Connection successful');
         this.db = this.client.db(database);
       })
       .catch((err) => {
@@ -19,7 +22,7 @@ class DBClient {
       });
   }
 
-  async isAlive() {
+  isAlive() {
     return !!this.db;
   }
 
