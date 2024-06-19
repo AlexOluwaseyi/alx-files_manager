7f9f62fb5b9d9a65a153dbdcf7eea909c5c860b8
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2024-06-18 22:37:38 -0700

    fixed issues in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController.js.old'
+import FilesController from '../controllers/FilesController.js'
 
 const router = express.Router();
 
@@ -13,7 +13,7 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', AuthController.getMe);
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
-router.get('files/:id', FilesController.getShow);
-router.get('files', FilesController.getIndex);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 module.exports = router;
